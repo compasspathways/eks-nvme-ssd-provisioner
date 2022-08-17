4e65e4dc7feed6441e2cfdeeda9c0a505fb3598f
Tamás Kádár <ktamas@ktamas.com>
2022-08-17 15:54:17 +0100

    new region

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 aws ecr create-repository --repository-name eks-nvme-ssd-provisioner
 docker buildx create --use --name crossbuild
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 344142597729.dkr.ecr.us-east-1.amazonaws.com
-docker buildx build --platform linux/amd64,linux/arm64 -t 344142597729.dkr.ecr.us-east-1.amazonaws.com/eks-nvme-ssd-provisioner:latest --push -f Dockerfile.alpine .
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 344142597729.dkr.ecr.eu-west-2.amazonaws.com
+docker buildx build --platform linux/amd64,linux/arm64 -t 344142597729.dkr.ecr.eu-west-2.amazonaws.com/eks-nvme-ssd-provisioner:latest --push -f Dockerfile.alpine .
